d98faed2ae2da382d21627ec8c14013547db128f
AOE-Dylan <dylantt2000@gmail.com>
2017-03-13 08:44:58 -1000

    first 4 methods

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ console.log("Reality Check");
 var idElement = document.getElementById('use-id-method');
 console.log(idElement);
 
+//changing HTML elements
+idElement.innerHTML = "I haz been changed";
+
 
 //console.log(document.getElementsByTagName('script'));
 var tagName = document.getElementsByTagName('script');
 console.log(tagName);
 
-//console.log(document.getElementsByClassName('use-class-method'));
+//sconsole.log(document.getElementsByClassName('use-class-method'));
 var classMethod = document.getElementsByClassName('use-class-method');
 console.log(classMethod);
+
+//changing class elements
+classMethod[1].innerHTML = "I haz been changed by Class name and array target!!!";
