abd53f49ea406b1e72f9fc1e875cb8f05ab8fa86
AOE-Dylan <dylantt2000@gmail.com>
2017-03-13 08:29:02 -1000

    first 2 methods

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 console.log("Reality Check");
 
-function function_name(argument) {
-  // body...
-}
+//console.log(document.getElementById('use-id-method'));
+var idElement = document.getElementById('use-id-method');
+console.log(idElement);
+
+
+//console.log(document.getElementsByTagName('script'));
+var tagName = document.getElementsByTagName('script');
+console.log(tagName);
+
+
